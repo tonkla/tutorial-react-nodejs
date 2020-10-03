6cd26cb33d84bf455fee9b83d16be59541bedb3a
Surakarn Samkaew <tonkla@gmail.com>
2020-10-03 11:48:48 +0700

    Add TailwindCSS

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import Home from './pages/Home'
+import './styles/tailwind-output.css'
 
 export default function App() {
   return (
---
+++ front-end/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: false,
+  theme: {},
+  variants: {},
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+}
