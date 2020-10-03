24dfc14f9a8b12c30a4dff45985d3385a57cb335
Surakarn Samkaew <tonkla@gmail.com>
2020-10-03 15:34:36 +0700

    Add login logic

--- front-end/src/pages/Login.tsx
+++ front-end/src/pages/Login.tsx
@@ -1,12 +1,27 @@
 import { Button, Input } from 'antd'
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const history = useHistory()
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(e.target.value)
+  }
+
+  function handleClick() {
+    const emailRegex = /(.+)@(.+){2,}\.(.+){2,}/
+    if (emailRegex.test(email)) {
+      history.push('/')
+    }
+  }
+
   return (
     <div className="flex items-center justify-center">
       <div className="mt-10">
-        <Input size="large" placeholder="Email" />
-        <Button size="large" type="primary" className="mt-4 w-full">
+        <Input size="large" placeholder="Email" onChange={handleChange} />
+        <Button size="large" type="primary" className="mt-4 w-full" onClick={handleClick}>
           Log in
         </Button>
       </div>
