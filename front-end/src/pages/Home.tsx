4a1ba8538a52f3695e59f9f82d25aa8abaf95a04
Surakarn Samkaew <tonkla@gmail.com>
2020-10-04 18:37:05 +0700

    Clean

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import { useStoreState } from '../store'
 
 export default function Home() {
-  const userRole = useStoreState((s) => s.userRoleState.userRole)
-  console.log(userRole)
   return <Layout>Home</Layout>
 }
