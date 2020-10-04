ac9ac484cb6bf737e2db34b8a29edc9e5bc4beb1
Surakarn Samkaew <tonkla@gmail.com>
2020-10-04 09:47:06 +0700

    Add rules

--- front-end/src/pages/Register.tsx
+++ front-end/src/pages/Register.tsx
@@ -14,14 +14,22 @@ export default function Register() {
     console.log('Error', error)
   }
 
+  const emailRegex = /(.+)@(.+){2,}\.(.+){2,}/
+
   return (
     <div className="flex items-center justify-center">
       <div className="mt-10 w-64">
         <Form onFinish={onFinish} onFinishFailed={onFinishFailed}>
-          <Form.Item>
+          <Form.Item
+            name="email"
+            rules={[
+              { required: true, message: 'Email is required' },
+              { pattern: emailRegex, message: 'Email is incorrect' },
+            ]}
+          >
             <Input size="large" placeholder="Email" onChange={handleChange} />
           </Form.Item>
-          <Form.Item>
+          <Form.Item name="password" rules={[{ required: true, message: 'Password is required' }]}>
             <Input size="large" type="password" placeholder="Password" onChange={handleChange} />
           </Form.Item>
           <Form.Item>
