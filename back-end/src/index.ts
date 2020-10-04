f2e7886b4fe7d6ef0b1ff9fbb3127fd43e575b35
Surakarn Samkaew <tonkla@gmail.com>
2020-10-04 10:39:43 +0700

    Add Jest

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,17 +1,16 @@
-import cors from '@koa/cors'
+// import cors from '@koa/cors'
 import Koa from 'koa'
-import bodyParser from 'koa-bodyparser'
+// import bodyParser from 'koa-bodyparser'
 import Router from 'koa-tree-router'
 
-const app = new Koa()
 const r = new Router()
-// r.get(`/ping`, (ctx) => {
-//   ctx.body = 'pong'
-// })
+r.get(`/ping`, (ctx) => {
+  ctx.body = 'pong'
+})
 
 const port = 8080
-app
-  .use(cors())
-  .use(bodyParser())
+new Koa()
+  // .use(cors())
+  // .use(bodyParser())
   .use(r.routes())
   .listen({ port }, async () => console.log(`🚀 API launched on port ${port}`))
