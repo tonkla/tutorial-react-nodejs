09500b4d81ee9b6d47fa4c9cbe9b55e25b91ca37
Surakarn Samkaew <tonkla@gmail.com>
2020-10-04 15:57:56 +0700

    Decorate layout

--- front-end/src/components/Layout.tsx
+++ front-end/src/components/Layout.tsx
@@ -1,22 +1,48 @@
-import { DashboardOutlined } from '@ant-design/icons'
-import { Layout, Menu } from 'antd'
-import React from 'react'
+import { DashboardOutlined, MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons'
+import { Button, Layout, Menu } from 'antd'
+import React, { useState } from 'react'
+import { useStoreActions, useStoreState } from '../store'
 
 export default function MainLayout({ children }: React.ComponentProps<'div'>) {
+  const [collapsed, setCollapsed] = useState(false)
+
+  const user = useStoreState((s) => s.userState.user)
+  const logOut = useStoreActions((a) => a.userState.logOut)
+
   const { Header, Content, Sider } = Layout
+
   return (
-    <Layout className="h-screen">
-      <Sider>
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1" icon={<DashboardOutlined />}>
-            Dashboard
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout>
-        <Header></Header>
-        <Content>{children}</Content>
+    user && (
+      <Layout className="h-screen">
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<DashboardOutlined />}>
+              Dashboard
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header className="px-10 bg-white">
+            <div className="flex">
+              <div className="flex items-center">
+                {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                  className: 'trigger',
+                  onClick: () => {
+                    setCollapsed(!collapsed)
+                  },
+                })}
+              </div>
+              <div className="flex-1 flex flex-row justify-end items-center">
+                <div>{user.email}</div>
+                <Button type="link" onClick={() => logOut()}>
+                  Log out
+                </Button>
+              </div>
+            </div>
+          </Header>
+          <Content className="p-10">{children}</Content>
+        </Layout>
       </Layout>
-    </Layout>
+    )
   )
 }
--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,25 +1,9 @@
-import { Button } from 'antd'
 import React from 'react'
 import { Redirect } from 'react-router-dom'
 import Layout from '../components/Layout'
-import { useStoreActions, useStoreState } from '../store'
+import { useStoreState } from '../store'
 
 export default function Home() {
   const user = useStoreState((s) => s.userState.user)
-  const logOut = useStoreActions((a) => a.userState.logOut)
-
-  return (
-    <div>
-      {!user ? (
-        <Redirect to="/login" />
-      ) : (
-        <Layout>
-          <div>{user.email}</div>
-          <Button type="link" onClick={() => logOut()}>
-            Log out
-          </Button>
-        </Layout>
-      )}
-    </div>
-  )
+  return <div>{!user ? <Redirect to="/login" /> : <Layout>Home</Layout>}</div>
 }
